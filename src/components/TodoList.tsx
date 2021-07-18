e507d32d3d63863185a5b0123c0622797d9daf99
JulioBicharra <julio.bicharra.cps@elo.com.br>
2021-07-18 10:42:29 -0300

    projeto de estudo reactjs

---
+++ src/components/TodoList.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { useContext } from "react";
+import { TodoContext } from "../contexts/TodoContext";
+import { TodoContextType } from "../contexts/TodoContextType";
+import TodoListItem from "./TodoListItem";
+
+const TodoList = () => {
+
+    const {todos} = useContext<TodoContextType>(TodoContext);
+
+    return (
+        <table className="uk-table">
+            <caption>Lista de tarefas</caption>
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Tarefa</th>
+                    <th>Feito</th>
+                </tr>
+            </thead>
+            <tbody>
+            {
+                todos?.map(
+                    todo => (
+                        <TodoListItem key={todo.id} todo={todo}></TodoListItem>
+                    )
+                )
+            }
+            </tbody>
+        </table>
+    )
+}
+
+export default TodoList;
---
+++ src/contexts/TodoContext.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
+import { createContext } from "react";
+import { Todo } from "../Models/Todo";
+import { get, save } from "../services/TodoService";
+import { TodoContextType } from "./TodoContextType";
+
+export const TodoContext = createContext<TodoContextType>({
+    todos: [],
+    addTodo: () => {},
+    removeTodo: () => {},
+    toggle: () => {}
+    });
+
+const TodoProvider = (props : any) => {
+
+    const  [todos, setTodos] = useState<Todo[]>(get);
+
+    // useEffect tem a função de monitorar o estado de um objeto
+    // e fazer alguma ação sempre que o objeto monitorado sofrer alteração 
+    useEffect(() => {
+        save(todos);
+    }, [todos]);
+
+
+    const  addTodo =(title: string) => {
+        const todo: Todo = { id: todos.length + 1, title: title, done: false}
+        setTodos([...todos, todo])
+    }
+
+    const  removeTodo =(todo: Todo) => {
+        const index = todos.indexOf(todo);
+        setTodos(todos.filter((_, i) => i !== index));
+    }
+
+    const  toggle =(todo: Todo) => {
+        const index = todos.indexOf(todo);
+        todos[index].done = !todo.done;
+        setTodos([...todos]);
+    }
+
+
+    return (
+        <TodoContext.Provider value={{todos, addTodo, removeTodo, toggle}}>
+            {props.children}
+        </TodoContext.Provider>
+    );
+};
+
+export default TodoProvider;
